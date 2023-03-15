b017d584a057071b18d5f27058870a5398382c03
Eizo <Krpubgmsn@gmail.com>
2023-03-15 12:07:51 +0545

    first python file is uploaded

---
+++ cs50 program01.py
@@ -0,0 +1,9 @@
+#In terms of getting a sequence first digit 
+#or first word we user
+name = ['manish','anish','ronish'] #this is a sequece of number
+#this statement below prints the first name in the list
+print(name[0])
+# list - sequence of mutable values 
+# tuple -sequence of mutable values
+# set - collection of unique values 
+# dec collection of key-value pairs
